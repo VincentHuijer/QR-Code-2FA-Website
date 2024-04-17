1acc99f0cff57eef04b3ec2d1f03b1f7f09386fe
Helloguis <94686622+VincentHuijer@users.noreply.github.com>
2024-04-17 23:40:16 +0200

    Update Homepage.js

--- src/views/Homepage.js
+++ src/views/Homepage.js
@@ -6,6 +6,8 @@ import { supabase } from "../lib/helper/supabaseClient";
 const Homepage = () => {
   const { session, setSession } = useContext(SessionContext);
 
+
+  //goal is to remove thise useEffect and use the useContext for session
   useEffect(() => {
     supabase.auth.getSession().then(({ data: { session } }) => {
       setSession(session)
