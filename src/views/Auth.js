37cb5cfe33d56fbba9ba0452eb0d42a3fe90b4cd
Helloguis <94686622+VincentHuijer@users.noreply.github.com>
2024-04-19 13:53:12 +0200

    Update Auth.js

--- src/views/Auth.js
+++ src/views/Auth.js
@@ -48,39 +48,43 @@ const Auth = () => {
   return (
     <div className="flex flex-col items-center bg-[#1C1C1E]">
     <div className=" w-9/12">
-    <img src={FitnessLogo} alt="FitnessLogo" className="mt-10 rounded-full border-5 border-white"/>
-      <div className="text-5xl font-bold text-white text-center">MoggingFitness</div>
-      <div className="text-4xl font-bold text-white text-center">Login</div>
-        <div>
-          <label className="text-[#9CA3AF] font-bold">Email</label>
+    <img src={FitnessLogo} alt="FitnessLogo" className="mt-10 rounded-full border-5 border-white w-50 h-50"/>
+      <div className="text-4xl font-bold text-white text-center mt-5">MoggingFitness</div>
+      <div className="text-3xl font-bold text-white text-center mt-3">Login</div>
+        <div className="mt-5">
+          <label className="text-[#9CA3AF] font-bold mt-2">Enter Email</label>
           <input
-            className='w-full placeholder-[#9CA3AF] text-white bg-[#1C1C1E] border-b-2 border-[#9CA3AF]'
+            className='w-full placeholder-[#9CA3AF] text-white bg-[#1C1C1E] mt-1'
             type="email"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
-            placeholder="enter email"
+            placeholder="Enter Email"
           />
+          <div className="border-b-2 border-[#9CA3AF] mt-1"/>
         </div>
-        <div>
-          <label>Password:</label>
+        <div className="mt-5">
+          <label className="text-[#9CA3AF] font-bold mt-2">Enter Password</label>
           <input
-            className='border border-black w-full outline-none p-2'
+            className='w-full placeholder-[#9CA3AF] text-white bg-[#1C1C1E] '
             type="password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
-            placeholder="password"
+            placeholder="Enter Password"
           />
+          <div className="border-b-2 border-[#9CA3AF] mt-1">
+
+          </div>
         </div>
-        <button className='bg-[#5BE432] w-full mt-10 rounded-md' onClick={signInWithEmail} disabled={loading}>
-            {loading ? 'Logging in...' : 'Log in'}
-          </button>
-        <div>
-          <button onClick={() => supabase.auth.signOut()} disabled={loading}>
+        <button className='bg-[#2CB3FC] w-full mt-10 p-2 rounded-sm' onClick={signInWithEmail} disabled={loading}>
+          {loading ? 'Logging in...' : 'Log in'}
+        </button>
+        <div className="mb-24">
+          <button className='bg-[#2CB3FC] w-full mt-10 p-2 rounded-sm' onClick={() => supabase.auth.signOut()} disabled={loading}>
             logout  
           </button>
         </div>
-        <div>
-          {session ? 'authenticated ✔️ '  : 'not authenticated ❌'}
+        <div className="text-2xl text-white" >
+          status:  {session ? 'authenticated ✔️ '  : 'not authenticated ❌'}
         </div>
         {session &&
           <EnrollMFA 
