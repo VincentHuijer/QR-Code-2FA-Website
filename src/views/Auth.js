4bd6b4dcb1cdc42cb33dc74db1475382ee0a73f7
Helloguis <94686622+VincentHuijer@users.noreply.github.com>
2024-04-19 12:50:57 +0200

    WIP designing auth page

--- src/views/Auth.js
+++ src/views/Auth.js
@@ -2,6 +2,7 @@ import { useContext, useEffect, useState } from "react";
 import { supabase } from "../lib/helper/supabaseClient";
 import { SessionContext } from "./components/contexts/SessionContext";
 import { EnrollMFA } from "./components/EnrollMFA";
+import FitnessLogo from "../media/FitnessLogo.png" 
 
 const Auth = () => {
   const [email, setEmail] = useState('')
@@ -45,11 +46,14 @@ const Auth = () => {
   }
 
   return (
-    <div>
-          <div className="text-4xl font-bold underline">Login</div>
+    <div className="flex flex-col items-center bg-[#1C1C1E]">
+    <div className=" w-9/12">
+    <img src={FitnessLogo} alt="FitnessLogo"/>
+      <div className="text-4xl font-bold ">Login</div>
         <div>
           <label>Email:</label>
           <input
+            className='border border-black w-full outline-none p-2'
             type="email"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
@@ -58,35 +62,31 @@ const Auth = () => {
         <div>
           <label>Password:</label>
           <input
+            className='border border-black w-full outline-none p-2'
             type="password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
           />
         </div>
+        <button className='bg-[#5BE432] w-full mt-10 rounded-md' onClick={signInWithEmail} disabled={loading}>
+            {loading ? 'Logging in...' : 'Log in'}
+          </button>
         <div>
-
-        <button onClick={signInWithEmail} disabled={loading}>
-          {loading ? 'Logging in...' : 'Log in'}
-        </button>
-
-        </div>
-        <div>
-        <button onClick={() => supabase.auth.signOut()} disabled={loading}>
-          logout  
-        </button>
-
+          <button onClick={() => supabase.auth.signOut()} disabled={loading}>
+            logout  
+          </button>
         </div>
         <div>
           {session ? 'authenticated ✔️ '  : 'not authenticated ❌'}
         </div>
-
-        <EnrollMFA 
-          onEnrolled={() => {}}
-          onCancelled={() => {}}
-        />
-
-      
-    </div>
+        {session &&
+          <EnrollMFA 
+            onEnrolled={() => {}}
+            onCancelled={() => {}}
+          />
+        }
+        </div>
+      </div>
   );
 }
 
